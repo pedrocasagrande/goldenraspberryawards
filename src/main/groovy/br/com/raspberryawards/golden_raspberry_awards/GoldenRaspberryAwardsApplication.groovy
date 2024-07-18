c3bf444bccc42bb646fb81e8ad947d533f737f14
Pedro H. Casagrande <60484216+pedrocasagrande@users.noreply.github.com>
2024-07-18 14:03:35 -0300

    Add files via upload

---
+++ src/main/groovy/br/com/raspberryawards/golden_raspberry_awards/GoldenRaspberryAwardsApplication.groovy
@@ -0,0 +1,13 @@
+package br.com.raspberryawards.golden_raspberry_awards
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GoldenRaspberryAwardsApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(GoldenRaspberryAwardsApplication, args)
+	}
+
+}
